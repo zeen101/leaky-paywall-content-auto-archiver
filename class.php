176a5b1cf94e57b304def4f43addea066a6798dd
Jeremy Green <greenhornet79@gmail.com>
2019-04-16 16:53:06 -0600

    update nag to work with restrictions class

--- class.php
+++ class.php
@@ -28,7 +28,7 @@ if ( ! class_exists( 'Leaky_Paywall_Content_Auto_Archiver' ) ) {
 			
 			add_action( 'admin_enqueue_scripts', array( $this, 'admin_wp_enqueue_scripts' ) );
 			
-			add_action( 'leaky_paywall_filter_is_restricted', array( $this, 'leaky_paywall_filter_is_restricted' ), 10, 2 );
+			add_action( 'leaky_paywall_filter_is_restricted', array( $this, 'leaky_paywall_filter_is_restricted' ), 10, 3 );
 
 			add_action( 'leaky_paywall_settings_form', array( $this, 'settings_div' ) );
 			add_action( 'leaky_paywall_update_settings', array( $this, 'update_settings_div' ) );
@@ -48,7 +48,7 @@ if ( ! class_exists( 'Leaky_Paywall_Content_Auto_Archiver' ) ) {
 			
 		}
 		
-		function leaky_paywall_filter_is_restricted( $is_restricted, $restrictions ) {
+		function leaky_paywall_filter_is_restricted( $is_restricted, $restrictions, $post_id ) {
 	
 			global $leaky_paywall_data, $blog_id;
 
@@ -78,7 +78,7 @@ if ( ! class_exists( 'Leaky_Paywall_Content_Auto_Archiver' ) ) {
 					if ( !current_user_can( 'manage_options' ) ) { //Admins can see it all
 					
 						// We don't ever want to block the login, subscription
-						if ( !is_page( array( $lp_settings['page_for_login'], $lp_settings['page_for_subscription'] ) ) ) {
+						if ( !is_page( array( $lp_settings['page_for_login'], $lp_settings['page_for_subscription'], $lp_settings['page_for_profile'], $lp_settings['page_for_register'] ) ) ) {
 												
 							global $post, $leaky_paywall;
 
@@ -91,7 +91,7 @@ if ( ! class_exists( 'Leaky_Paywall_Content_Auto_Archiver' ) ) {
 								$post_time = strtotime( $post->post_date );
 								
 								if ( $post_time <= $exp_time ) {
-								
+									
 									add_filter( 'the_content', array( $this, 'the_content_paywall' ), 999 );
 									$is_restricted = false; //This is an expired post, so we want to use the expired message
 								
@@ -126,9 +126,9 @@ if ( ! class_exists( 'Leaky_Paywall_Content_Auto_Archiver' ) ) {
 			
 			$message  = '<div id="leaky_paywall_message">';
 			if ( !is_user_logged_in() ) {
-				$message .= $leaky_paywall->replace_variables( stripslashes( $settings['subscribe_archive_login_message'] ) );
+				$message .= $this->replace_variables( stripslashes( $settings['subscribe_archive_login_message'] ) );
 			} else {
-				$message .= $leaky_paywall->replace_variables( stripslashes( $settings['subscribe_archive_upgrade_message'] ) );
+				$message .= $this->replace_variables( stripslashes( $settings['subscribe_archive_upgrade_message'] ) );
 			}
 			$message .= '</div>';
 		
@@ -136,6 +136,44 @@ if ( ! class_exists( 'Leaky_Paywall_Content_Auto_Archiver' ) ) {
 		
 			return apply_filters( 'leaky_paywall_content_archived_subscribe_or_login_message', $new_content, $message, $content );
 			
+		}
+
+		public function replace_variables( $message ) 
+		{
+			
+			$settings = get_leaky_paywall_settings();
+
+			if ( 0 === $settings['page_for_subscription'] )
+				$subscription_url = get_bloginfo( 'wpurl' ) . '/?subscription'; //CHANGEME -- I don't really know what this is suppose to do...
+			else
+				$subscription_url = get_page_link( $settings['page_for_subscription'] );
+
+			if ( 0 === $settings['page_for_profile'] )
+				$my_account_url = get_bloginfo( 'wpurl' ) . '/?my-account'; //CHANGEME -- I don't really know what this is suppose to do...
+			else
+				$my_account_url = get_page_link( $settings['page_for_profile'] );
+
+			$message = str_ireplace( '{{SUBSCRIBE_LOGIN_URL}}', $subscription_url, $message );
+			$message = str_ireplace( '{{SUBSCRIBE_URL}}', $subscription_url, $message );
+			$message = str_ireplace( '{{MY_ACCOUNT_URL}}', $my_account_url, $message );
+
+			if ( 0 === $settings['page_for_login'] )
+				$login_url = get_bloginfo( 'wpurl' ) . '/?login'; //CHANGEME -- I don't really know what this is suppose to do...
+			else
+				$login_url = get_page_link( $settings['page_for_login'] );
+
+			$message = str_ireplace( '{{LOGIN_URL}}', $login_url, $message );
+
+			//Deprecated
+			if ( !empty( $settings['price'] ) ) {
+				$message = str_ireplace( '{{PRICE}}', $settings['price'], $message );
+			}
+			if ( !empty( $settings['interval_count'] ) && !empty( $settings['interval'] ) ) {
+				$message = str_ireplace( '{{LENGTH}}', leaky_paywall_human_readable_interval( $settings['interval_count'], $settings['interval'] ), $message );
+			}
+
+			return $message;
+					
 		}
 				
 		/**
