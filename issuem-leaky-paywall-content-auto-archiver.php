07543773f97c9eb3e9ca02239334280ee2dfeb68
Jeremy Green <jeremygreen@Jeremys-MBP.hsd1.co.comcast.net>
2024-01-22 14:49:06 -0700

    add updater functionality

--- issuem-leaky-paywall-content-auto-archiver.php
+++ issuem-leaky-paywall-content-auto-archiver.php
@@ -39,21 +39,21 @@ define('LP_CAA_REL_DIR', 	dirname(LP_CAA_BASENAME));
 function leaky_paywall_content_auto_archiver_plugins_loaded()
 {
 
+	global $is_leaky_paywall, $which_leaky_paywall;
 	include_once(ABSPATH . 'wp-admin/includes/plugin.php');
 
-	if (
-		is_plugin_active('issuem-leaky-paywall/issuem-leaky-paywall.php')
-		|| is_plugin_active('leaky-paywall/leaky-paywall.php')
-	) {
+	if (is_plugin_active('issuem-leaky-paywall/issuem-leaky-paywall.php')) {
+		$is_leaky_paywall = true;
+		$which_leaky_paywall = '_issuem';
+	} else if (is_plugin_active('leaky-paywall/leaky-paywall.php')) {
+		$is_leaky_paywall = true;
+		$which_leaky_paywall = '';
+	} else {
+		$is_leaky_paywall = false;
+		$which_leaky_paywall = '';
+	}
 
-		global $leaky_paywall_data;
-
-		if (is_plugin_active('issuem-leaky-paywall/issuem-leaky-paywall.php')) {
-			$leaky_paywall_data = get_plugin_data(WP_PLUGIN_DIR . '/issuem-leaky-paywall/issuem-leaky-paywall.php');
-		} else {
-			$leaky_paywall_data = get_plugin_data(WP_PLUGIN_DIR . '/leaky-paywall/leaky-paywall.php');
-		}
-
+	if (!empty($is_leaky_paywall)) {
 		require_once('class.php');
 
 		// Instantiate the Pigeon Pack class
@@ -63,17 +63,33 @@ function leaky_paywall_content_auto_archiver_plugins_loaded()
 			$leaky_paywall_content_auto_archiver = new Leaky_Paywall_Content_Auto_Archiver();
 
 			require_once('includes/admin/settings.php');
+			require_once('includes/updates.php');
 			require_once('functions.php');
 
 			//Internationalization
 			load_plugin_textdomain('issuem-lp-caa', false, LP_CAA_REL_DIR . '/i18n/');
 		}
-	} else {
 
+		// Upgrade function based on EDD updater class
+		if (!class_exists('EDD_LP_Plugin_Updater')) {
+			include(dirname(__FILE__) . '/includes/EDD_LP_Plugin_Updater.php');
+		}
+
+		$license = new Leaky_Paywall_License_Key(LP_CAA_SLUG, LP_CAA_NAME);
+
+		$settings = $license->get_settings();
+		$license_key = trim($settings['license_key']);
+		$edd_updater = new EDD_LP_Plugin_Updater(ZEEN101_STORE_URL, __FILE__, array(
+			'version' 	=> LP_CAA_VERSION, // current version number
+			'license' 	=> $license_key,
+			'item_id'	=> 13792,
+			'author' 	=> 'Zeen101 Development Team'
+		));
+	} else {
 		add_action('admin_notices', 'leaky_paywall_content_auto_archiver_requirement_nag');
 	}
 }
-add_action('plugins_loaded', 'leaky_paywall_content_auto_archiver_plugins_loaded', 4815162342); //wait for the plugins to be loaded before init
+add_action('plugins_loaded', 'leaky_paywall_content_auto_archiver_plugins_loaded', 4815162344); //wait for the plugins to be loaded before init
 
 function leaky_paywall_content_auto_archiver_requirement_nag()
 {
